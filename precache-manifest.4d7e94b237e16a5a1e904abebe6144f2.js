e6ae6ec4a9ac4d2a681cedb6d8405c9541f7ace2
narton2 <rugged245@gmail.com>
2020-01-16 22:13:19 +0100

    Updates

---
+++ precache-manifest.4d7e94b237e16a5a1e904abebe6144f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95492f3d51be2c7e82b2c25e536334b4",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2075c5cd75730716fdf8",
+    "url": "/test/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "23b22e02333ca278790d",
+    "url": "/test/static/js/2.fd869087.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.fd869087.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2075c5cd75730716fdf8",
+    "url": "/test/static/js/main.71f6c56f.chunk.js"
+  },
+  {
+    "revision": "4ed7cf1bdcef7f416212",
+    "url": "/test/static/js/runtime-main.874278bd.js"
+  }
+]);
